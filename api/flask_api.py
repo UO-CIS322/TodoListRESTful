319284aaa449c0fce4774485e9044fbd631071b1
Ali Hassani <alihassanijr1998@gmail.com>
2023-03-07 09:48:30 -0800

    TodoList resource

--- api/flask_api.py
+++ api/flask_api.py
@@ -7,7 +7,8 @@ from flask import Flask
 from flask_restful import Api
 from mongoengine import connect
 
-from resources.todolists import TodoListResource
+from resources.todolist import TodoListResource
+from resources.todolists import TodoListsResource
 
 # Connect MongoEngine to mongodb
 connect(host=f"mongodb://{os.environ['MONGODB_HOSTNAME']}:27017/brevetsdb")
@@ -20,7 +21,8 @@ app.logger.setLevel(logging.DEBUG)
 api = Api(app)
 
 # Bind resources to paths here:
-api.add_resource(TodoListResource, "/api/todolists")
+api.add_resource(TodoListResource, "/api/todolist/<id>")
+api.add_resource(TodoListsResource, "/api/todolists")
 
 if __name__ == "__main__":
     # Run flask app normally
--- api/resources/todolists.py
+++ api/resources/todolists.py
@@ -1,12 +1,12 @@
 """
-Resource: TodoListResource
+Resource: TodoListsResource
 """
 from flask import Response, request
 from flask_restful import Resource
 
 from database.models import TodoList
 
-class TodoListResource(Resource):
+class TodoListsResource(Resource):
     def get(self):
         json_object = TodoList.objects().to_json()
         return Response(json_object, mimetype="application/json", status=200)
@@ -16,9 +16,10 @@ class TodoListResource(Resource):
         # This will fail if the request body is NOT a JSON.
         input_json = request.json
 
-        # Because input_json is a dictionary, we can do this:
-        title = input_json["title"] # Should be a string
-        items = input_json["items"] # Should be a list of dictionaries
+        ## Because input_json is a dictionary, we can do this:
+        #title = input_json["title"] # Should be a string
+        #items = input_json["items"] # Should be a list of dictionaries
+        #result = TodoList(title=title, items=items).save()
 
-        result = TodoList(title=title, items=items).save()
+        result = TodoList(**input_json).save()
         return {'_id': str(result.id)}, 200
